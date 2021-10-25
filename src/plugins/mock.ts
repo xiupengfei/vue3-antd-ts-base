4b16a3c93ac0e2287e2672fa4bdbb9f55231cb32
pengfei.xiu <15541647796@163.com>
2021-10-25 20:33:26 +0800

    upd: lintstaged

---
+++ src/plugins/mock.ts
@@ -0,0 +1,14 @@
+const mockServer = () => ({
+  name: 'configure-server',
+  configureServer(server) {
+    // 返回一个在内部中间件安装后
+    // 被调用的后置钩子
+    return () => {
+      server.middlewares.use((req, res, next) => {
+        // 自定义请求处理...
+      })
+    }
+  },
+})
+
+export default mockServer
